58ff51618dbbb3dfd4e8bed3d59bc60e6813290e
ipTIME GPL <iptime.gpl@guu.moe>
2018-05-22 14:05:32 +0900

    update

--- linux/linux-3.10/include/generated/compile.h
+++ linux/linux-3.10/include/generated/compile.h
@@ -1,6 +1,6 @@
-/* This file is auto generated, version 182 */
+/* This file is auto generated, version 210 */
 #define UTS_MACHINE "mips"
-#define UTS_VERSION "#182 Mon Aug 21 09:54:21 KST 2017"
+#define UTS_VERSION "#210 Mon Oct 30 18:39:11 KST 2017"
 #define LINUX_COMPILE_BY "rtlac"
 #define LINUX_COMPILE_HOST "ski"
 #define LINUX_COMPILER "gcc version 4.4.7 (Realtek MSDK-4.4.7 Build 2001) "
